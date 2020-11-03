ca0a5c52783810cf3c076e44358446cd9fdec46d
amal m <a.@hotmail.fr>
2020-11-03 10:04:31 +0100

    Ajout Search.js

--- doctolib/src/Component/Accueil.js
+++ doctolib/src/Component/Accueil.js
@@ -1,35 +1,63 @@
 import React, { Component } from 'react';
-import Recherche from './Recherche';
-
+import Search from './Search';
 
 
 class Accueil extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {  }
     }
+    render() { 
+        return ( 
+            <div className="container">
 
-
-
-    render() {
-        return (
             <div className="navbar navbar-expand-lg navbar-light bg-primary">
+
                 <ul className="navbar-nav mr-auto">
+
                     <li className="nav-item m-2">
+
                         <button class="btn btn-primary" type="submit">Doctolib</button>
+
                     </li>
+
                     <li className="nav-item m-2">
-                        <button class="btn btn-primary" type="submit">Vous êtes un professionnel?</button>
+
+                        <button class="btn btn-light" type="submit">Vous êtes un praticien ?</button>
+
                     </li>
-                    <li className="nav-item m-2">
-                        <button class="btn btn-primary" type="submit">Se connecter(gérer mes RDV)</button>
+
+                    <li className="nav-item m-2"  >
+
+                        <button class="btn btn-primary" type="submit">Se connecter</button>
+
                     </li>
+
+
 
                 </ul>
-              <recherche recherche={this.recherche}></recherche>
+
             </div>
-        );
+
+            <div>
+
+                <img src="https://media.licdn.com/dms/image/C4E1BAQE3OKeRJ-JuOw/company-background_10000/0?e=2159024400&v=beta&t=G7PxmUG516nuNeGrKqyOEDzmEy5rw2B_GfAbGyKk0_w" class="img-fluid" alt="Responsive image" />
+
+                <h1>Réservez une consultation physique chez un professionnel de santé</h1>
+
+            </div>
+
+            <div className="row m-5">
+
+                <Search onChange={(e) => { this.search(e.target.value) }} placeholder="Votre recherche" enterButton="Search" size="large" />
+
+            </div>
+
+
+
+        </div>
+         );
     }
 }
-
+ 
 export default Accueil;
---
+++ doctolib/src/Component/Search.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div classeName="container">
+                <div className="row">
+                    <input className="col form-control" onChange={this.changeSearch} placeholder="votre recherche"></input>
+                    <button onClick={this.valideClick} className="col-4 btn btn-primary">Valider</button>
+                </div>
+
+            </div>
+         );
+    }
+}
+ 
+export default Search ;
